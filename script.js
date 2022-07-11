6ce63c4409c797bdd8195d3b5605edf7e206a1fa
amir <kvanpieter@gmail.com>
2022-07-11 12:11:16 +0500

    dz15

---
+++ script.js
@@ -0,0 +1,27 @@
+var name = prompt('Введите имя:');
+console.log(name);
+
+var age = +prompt('Введите ваш возраст:');
+console.log(age);
+
+var question = 100 + 100;
+var answer = +prompt('Вычислите: 100 + 100')
+console.log('100 + 100 = ' + question + ' (Ваш ответ ' + answer + ')');
+
+var question = 250 - 150;
+var answer = +prompt('Вычислите: 250 - 150')
+console.log('250 - 150 = ' + question + ' (Ваш ответ ' + answer + ')');
+
+var question = 12 * 12;
+var answer = +prompt('Вычислите: 12 * 12')
+console.log('12 * 12 = ' + question + ' (Ваш ответ ' + answer + ')');
+
+var question = 82 / 2;
+var answer = +prompt('Вычислите: 82 / 2')
+console.log('82 / 2 = ' + question + ' (Ваш ответ ' + answer + ')');
+
+var question = 21 % 4;
+var answer = +prompt('Найдите остаток: 21 % 4')
+console.log('21 % 4 = ' + question + ' (Ваш ответ ' + answer + ')');
+
+alert('Перейдите в консоль чтобы сравнить ваши ответы с настоящими! ')
